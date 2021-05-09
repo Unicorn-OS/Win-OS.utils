e8e33daa3c24df55a792d12061aa8045b8e6131a
Dark Star <darkstar@unisocial.net>
2021-05-09 16:46:48 -0700

    2nd

---
+++ Windows Theme/Dark-Theme.ps1
@@ -0,0 +1,7 @@
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
+
+<#
+https://www.google.com/search?q=windows+dark+mode+powershell
+
+- http://joonro.github.io/blog/posts/windows-10-enable-dark-mode-posh/
+#>
